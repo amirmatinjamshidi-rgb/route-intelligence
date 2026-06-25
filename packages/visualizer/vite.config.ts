7136b6bd223d3613f32609ac6f8156d85bed2234
AmirMatin <amirmatinjamshidi@gmail.com>
2026-06-25 13:36:23 +0330

    eslint fix vite

--- packages/visualizer/vite.config.ts
+++ packages/visualizer/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, PluginOption } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() as PluginOption],
   server: { port: 3001 },
 });
